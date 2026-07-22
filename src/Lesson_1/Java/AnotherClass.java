4f42de104dbd38c1c629bbe0f6c4bbb1126f1bc0
Igor188 <igor_turdakov@mail.ru>
2026-07-22 23:29:25 +0400

    Lesson2

--- src/Lesson_1/Java/AnotherClass.java
+++ src/Lesson_1/Java/AnotherClass.java
@@ -3,11 +3,11 @@ package Lesson_1.Java;
 import org.junit.Test;
 
 public class AnotherClass {
-    @Test
+    @      Test
     public void test () {
         TestMain testMain = new TestMain();
 
-        //System.out.println(TestMain3_Access_modifier.hi);//
+        // S      ystem.out.println(TestMain3_Access_modifier.hi);//
        // System.out.println(TestMain3_Access_modifier.bro);//
     }
 }
